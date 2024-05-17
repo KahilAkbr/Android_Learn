0e8a2b4b86d6ad346ad7e29cd47aceddeee912e2
KahilAkbr <kahil.akbar@gmail.com>
2024-05-17 23:28:12 +0700

    testing - room

---
+++ AndroidIntermediate/LatihanAdvancedTesting - room/build.gradle.kts
@@ -0,0 +1,10 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.11" apply false
+
+    //Kover
+    id("org.jetbrains.kotlinx.kover") version "0.5.0"
+}
+
