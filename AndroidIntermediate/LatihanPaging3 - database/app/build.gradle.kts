72ebc206db92096ebb1682996bf15344cbd1908e
KahilAkbr <kahil.akbar@gmail.com>
2024-05-23 01:07:31 +0700

    firebase auth

--- AndroidIntermediate/LatihanPaging3 - database/app/build.gradle.kts
+++ AndroidIntermediate/LatihanPaging3 - database/app/build.gradle.kts
@@ -67,8 +67,11 @@ dependencies {
     implementation(libs.room.paging)
 
     //mock, livedata, courutine testing
-    testImplementation("androidx.arch.core:core-testing:2.1.0") // InstantTaskExecutorRule
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.1") //TestDispatcher
-    testImplementation("org.mockito:mockito-core:3.12.4")
-    testImplementation("org.mockito:mockito-inline:3.12.4")
+    androidTestImplementation(libs.androidx.core.testing) //InstantTaskExecutorRule
+    androidTestImplementation(libs.kotlinx.coroutines.test) //TestDispatcher
+
+    testImplementation(libs.androidx.core.testing) // InstantTaskExecutorRule
+    testImplementation(libs.kotlinx.coroutines.test) //TestDispatcher
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.inline)
 }
