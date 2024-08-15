920ab85428a3b20c81af419679a906e0a4515cf8
Kahil Akbar <kahil.akbar@gmail.com>
2024-08-15 15:28:55 +0700

    dagger

---
+++ AndroidExpert/ProjectDagger/app/src/main/java/com/dicoding/tourismapp/di/AppScope.kt
@@ -0,0 +1,8 @@
+package com.dicoding.tourismapp.di
+
+import java.lang.annotation.RetentionPolicy
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class AppScope
